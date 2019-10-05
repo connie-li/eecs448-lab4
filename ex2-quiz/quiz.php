79b74eb5eea3d5eba269f12c0742e2b6cf3cb997
Connie <constanceli@ku.edu>
2019-10-04 21:31:40 -0500

    feat(ex2): calculate score & add quiz result output
    and add .htaccess
    
    new file:   .htaccess

--- ex2-quiz/quiz.php
+++ ex2-quiz/quiz.php
@@ -1,9 +1,51 @@
 <?php
+	$score = 0;
     $q1answer = $_POST["1"];
     $q2answer = $_POST["2"];
     $q3answer = $_POST["3"];
     $q4answer = $_POST["4"];
     $q5answer = $_POST["5"];
 
-    echo '<div>You answered: ', $q1answer, ', ' $q2answer, '</div>';
+    if($q1answer == "Sandy")
+    {
+    	$score++;
+    }
+    if($q2answer == "Ignotus")
+    {
+    	$score++;
+    }
+    if($q3answer == "Japanese Wood Pigeon")
+    {
+    	$score++;
+    }
+    if($q4answer == "Temple of Mara")
+    {
+    	$score++;
+    }
+    if($q5answer == "March 2019")
+    {
+    	$score++;
+    }
+
+    echo '<div>You scored ', $score / 5, ' with ', $score, ' correct answers.</div>';
+
+    echo "<div><p>Question 1: Who is Basil the basil's best friend?</p>";
+    echo '<p>You answered: ', $q1answer, '</p>';
+    echo '<p>Correct answer: Sandy</p></div>';
+
+    echo "<div><p>Question 2: Which Peverell brother owned the Invisibility Cloak?</p>";
+    echo '<p>You answered: ', $q2answer, '</p>';
+    echo '<p>Correct answer: Ignotus</p></div>';
+
+    echo '<div><p>Question 3: Which species of bird belongs to the <em>Columbidae</em> family?</p>';
+    echo '<p>You answered: ', $q3answer, '</p>';
+    echo '<p>Correct answer: Japanese Wood Pigeon</p></div>';
+
+    echo '<div><p>Question 4: Which temple should you visit when you want to get married?</p>';
+    echo '<p>You answered: ', $q4answer, '</p>';
+    echo '<p>Correct answer: Temple of Mara</p></div>';
+
+    echo '<div><p>Question 5: When did Basil the basil sprout?</p>';
+    echo '<p>You answered: ', $q5answer, '</p>';
+    echo '<p>Correct answer: March 2019</p></div>';
 ?>
