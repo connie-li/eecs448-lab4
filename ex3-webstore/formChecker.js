bce2845fbe593fa55a640fe78112b9e386e16f89
Connie <constanceli@ku.edu>
2019-10-05 23:45:24 -0500

    feat(ex3): validate product quantities

---
+++ ex3-webstore/formChecker.js
@@ -0,0 +1,13 @@
+/** Check that the given quantity is valid, ie. greater than or equal to 0 and not blank.
+ * @param {string} inputName - the name attribute of the input being checked.
+ * @return {boolean} true if the quantity is valid, else false.
+ */
+function checkQuantity(inputName) {
+    let quantity = document.getElementById("product-chooser").elements[inputName].value;
+    if(quantity < 0 || quantity == "") {
+        return false;
+    }
+    else {
+        return true;
+    }
+}
