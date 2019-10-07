05e5ddc7b865179f3e829575229145a087865c6b
Connie <constanceli@ku.edu>
2019-10-07 06:21:34 -0500

    feat(ex3): add styles for the receipt page

--- ex3-webstore/customerBackend.php
+++ ex3-webstore/customerBackend.php
@@ -3,7 +3,7 @@
 	$quantP2 = $_POST["quant-p2"];
 	$quantP3 = $_POST["quant-p3"];
 	$p1Subtotal = $quantP1 * 10.00;
-	$p2Subtotal = $quantP2 * 7.50;
+	$p2Subtotal = $quantP2 * 6;
 	$p3Subtotal = $quantP3 * 14.00;
 
 	$ship = $_POST["ship"];
@@ -29,20 +29,20 @@
 		<head>
 			<meta charset="utf-8">
 			<title>Exercise 3: Web Store - Lab 4, EECS 448</title>
-			<link rel="stylesheet" type="text/css" href="styles.css">
+			<link rel="stylesheet" type="text/css" href="receipt.css">
 		</head>
 		<body>
-			<h4>EECS 448, Exercise 3: Web Store</h4>
+			<h4>EECS 448, Lab 4, Exercise 3: Web Store</h4>
 			<div id="content"><h1>Thank you for your order!</h1><div class="inner-content">';
 	echo '<p class="p-block">Username: ', $_POST["username"], '</p><p class="p-block">Password: ', $_POST["password"], '</p>';
 
 	// receipt table
 	echo '<div><table><caption>Your receipt: </caption>
-		<tr><td></td><td>Quantity</td><td>Cost per each</td><td>Subtotal</td></tr>';
+		<tr><td></td><th>Quantity</th><th>Cost per each</th><th>Subtotal</th></tr>';
 	// product 1
 	echo '<tr><th scope="row">Product 1</th><td>', $quantP1, '</td><td>$10.00</td><td>$', $p1Subtotal, '</td></tr>';
 	// product 2
-	echo '<tr><th scope="row">Product 2</th><td>', $quantP2, '</td><td>$7.50</td><td>$', $p2Subtotal, '</td></tr>';
+	echo '<tr><th scope="row">Product 2</th><td>', $quantP2, '</td><td>$6.00</td><td>$', $p2Subtotal, '</td></tr>';
 	// product 3
 	echo '<tr><th scope="row">Product 3</th><td>', $quantP3, '</td><td>$14.00</td><td>$', $p3Subtotal, '</td></tr>';
 	echo '<tr><th scope="row">Shipping</th><td colspan="2">', $shipType, '</td><td>$', $shipCost, '</td></tr>';
